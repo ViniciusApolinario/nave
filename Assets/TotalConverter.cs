0081f6f01d9cf78276ace01817f952ef615296e8
Matheus Avellar <MatheusAvellar@users.noreply.github.com>
2016-10-15 13:47:49 -0300

    Add Unity project - Part 3 / ?

---
+++ Assets/TotalConverter.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class TotalConverter : MonoBehaviour {
+
+	[SerializeField]
+	private Text mValue;
+	[SerializeField]
+	private Text currency;
+
+	private float cValue;
+	private bool convertBase;
+
+	void Start() {
+		convertBase = false;
+		cValue = 0;
+	}
+
+	public void changeCurrency() {
+		if (!convertBase) {
+			currency.text = "USD";
+			float tmp = float.Parse (mValue.text);
+			Debug.Log (tmp);
+			tmp *= 0.31f;
+			tmp = convertToMoney (tmp);
+			mValue.text = tmp.ToString ();
+			convertBase = true;
+		} else {
+			currency.text = "BRL";
+			float tmp = float.Parse (mValue.text);
+			Debug.Log (tmp);
+			tmp *= 3.2f;
+			tmp = convertToMoney (tmp);
+			mValue.text = tmp.ToString ();
+			convertBase = false;
+		}
+	}
+
+	public float convertToMoney(float f) {
+		return Mathf.Floor(f * 100) / 100;
+	}
+
+	public void AddValue(float value) {
+		cValue += value;
+		mValue.text = cValue.ToString ();
+	}
+
+	/*public void RemoveValue(float value) {
+		cValue -= value;
+		mValue.text = cValue.ToString ();
+	}*/
+}
