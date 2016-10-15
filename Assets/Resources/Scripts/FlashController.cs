f1776c93b7d101100ba2c1e320126284e8b54c45
Matheus Avellar <MatheusAvellar@users.noreply.github.com>
2016-10-15 13:50:49 -0300

    Add Unity project - Part 5 / ?

---
+++ Assets/Resources/Scripts/FlashController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using Vuforia;
+using System.Collections;
+
+public class FlashController : MonoBehaviour {
+
+	private bool canActive;
+
+	void Start() {
+		canActive = true;
+	}
+
+	public void FlashBehaviour() {
+		if (canActive) {
+			CameraDevice.Instance.SetFlashTorchMode (true);
+			canActive = false;
+		} else {
+			CameraDevice.Instance.SetFlashTorchMode (false);
+			canActive = true;
+		}
+	}
+
+	public void Focus() {
+		CameraDevice.Instance.SetFocusMode (CameraDevice.FocusMode.FOCUS_MODE_TRIGGERAUTO);
+	}
+}
